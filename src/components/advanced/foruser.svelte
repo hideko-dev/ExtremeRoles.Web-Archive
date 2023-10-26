ef1c547ff83e811e7e93c95e9e1dbca5f63fa517
Hideko <hideko332200@gmail.com>
2023-10-26 17:43:48 +0900

    deployyyyy

--- src/components/advanced/foruser.svelte
+++ src/components/advanced/foruser.svelte
@@ -1,46 +1,50 @@
 <script>
     import {createBetween, createSingleBool} from "../../lib/scroll.js";
     import Fa from "svelte-fa";
-    let opacity = 1;
     import { fade } from "svelte/transition";
     import {faBolt, faFire, faGear} from "@fortawesome/free-solid-svg-icons";
     const l1 = createBetween(1000, 1800);
     const l2 = createBetween(1200, 1800);
     const l3 = createBetween(1400, 1800);
     let icon = faBolt;
-    let parcent = 50;
-    function toggleOpacity() {
-        opacity = 0;
-        setTimeout(() => {
-            opacity = 1
-        }, 300);
+    let parcent = 3;
+    let rotate = 0;
+    function rotation() {
+        if(rotate === 0){
+            rotate = 360
+        } else {
+            rotate = 0
+        }
     }
     $: {
         if ($l1 && $l2 && $l3) {
-            toggleOpacity()
-            parcent = 44;
+            rotation()
+            parcent = 10;
             icon = faGear
         } else if ($l1 && $l2) {
-            toggleOpacity()
-            parcent = 46.5;
+            rotation()
+            parcent = 6.8;
             icon = faFire
         } else if ($l1) {
-            parcent = 49.5;
+            rotation()
+            parcent = 3.5;
             icon = faBolt
         }
     }
 </script>
 
 <div class="user">
-    <div class="img" style={`opacity: ${$l1 ? 1 : 0}`}>
-        <div class="icon" style="opacity: {opacity}">
-            <Fa fw icon={icon}/>
+    <div class="contents">
+        <div class="img" style={`opacity: ${$l1 ? 1 : 0}`}>
+            <div class="icon" style="rotate: {rotate}deg">
+                <Fa fw icon={icon}/>
+            </div>
         </div>
-    </div>
-    <div class="texts" style="top: {parcent}%">
-        <p style={`opacity: ${$l1 ? 1 : 0}`}>For Advanded User</p>
-        <p style={`opacity: ${$l2 ? 1 : 0}`}>80+ Roles</p>
-        <p style={`opacity: ${$l3 ? 1 : 0}`}>700+ Options</p>
+        <div class="texts" style="height: {parcent}rem">
+            <p style={`opacity: ${$l1 ? 1 : 0}`}>For Advanded User</p>
+            <p style={`opacity: ${$l2 ? 1 : 0}`}>80+ Roles</p>
+            <p style={`opacity: ${$l3 ? 1 : 0}`}>700+ Options</p>
+        </div>
     </div>
 </div>
 
@@ -51,10 +55,17 @@
         justify-content: center;
         align-items: center;
     }
-    .texts {
+    .contents {
         position: fixed;
-        left: 60%;
+        top: 42%;
+        left: 50%;
         transform: translateX(-50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .texts {
+        margin-left: 1rem;
         font-family: 'Inter', sans-serif;
         font-weight: 600;
         font-size: 40px;
@@ -64,19 +75,16 @@
     .texts p {
         transition: all 0.4s ease;
         margin-inline: 10px;
+        margin-block: 5px;
     }
     .img {
-        position: fixed;
         display: flex;
         justify-content: center;
         align-items: center;
-        left: 44%;
-        top: 42%;
         width: 12rem;
         height: 12rem;
-        background: #282828;
+        background: #1f1f1f;
         border-radius: 10px;
-        transform: translateX(-50%);
         font-family: 'Inter', sans-serif;
         font-size: 100px;
         transition: all 0.3s ease;
