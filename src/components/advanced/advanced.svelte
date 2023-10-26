5557c55371363597892ef9d01be9585f2cbbb1c3
Hideko <hideko332200@gmail.com>
2023-10-26 17:32:08 +0900

    deploy

--- src/components/advanced/advanced.svelte
+++ src/components/advanced/advanced.svelte
@@ -1,9 +1,7 @@
 <script>
     import User from "./foruser.svelte"
-    import Feature from "./features.svelte";
 </script>
 
 <div class="advanced">
     <User/>
-    <Feature/>
 </div>
--- src/components/advanced/foruser.svelte
+++ src/components/advanced/foruser.svelte
@@ -1,53 +1,87 @@
 <script>
     import {createBetween, createSingleBool} from "../../lib/scroll.js";
-    const text_for = createBetween(900, 1200);
-    const text_advanced = createBetween(1000, 1300);
-    const text_user = createBetween(1100, 1400);
-    // let fontsize = 70;
-    //
-    // $: {
-    //     if ($text_for && $text_advanced && $text_user) {
-    //         fontsize = 55;
-    //     } else if ($text_for && $text_advanced) {
-    //         fontsize = 60;
-    //     } else if ($text_for) {
-    //         fontsize = 65;
-    //     } else {
-    //         fontsize = 70;
-    //     }
-    // }
+    import Fa from "svelte-fa";
+    let opacity = 1;
+    import { fade } from "svelte/transition";
+    import {faBolt, faFire, faGear} from "@fortawesome/free-solid-svg-icons";
+    const l1 = createBetween(1000, 1800);
+    const l2 = createBetween(1200, 1800);
+    const l3 = createBetween(1400, 1800);
+    let icon = faBolt;
+    let parcent = 50;
+    function toggleOpacity() {
+        opacity = 0;
+        setTimeout(() => {
+            opacity = 1
+        }, 300);
+    }
+    $: {
+        if ($l1 && $l2 && $l3) {
+            toggleOpacity()
+            parcent = 44;
+            icon = faGear
+        } else if ($l1 && $l2) {
+            toggleOpacity()
+            parcent = 46.5;
+            icon = faFire
+        } else if ($l1) {
+            parcent = 49.5;
+            icon = faBolt
+        }
+    }
 </script>
 
 <div class="user">
-    <p style="position: fixed; color: white; left: 0; top: 0;">でばっぐ用: {$text_for} , {$text_advanced}, {$text_user}</p>
-    <div class="texts">
-        <p style={`opacity: ${$text_for ? 1 : 0}`}>For</p>
-        <p style={`opacity: ${$text_advanced ? 1 : 0}`}>Advanced</p>
-        <p style={`opacity: ${$text_user ? 1 : 0}`}>Users</p>
+    <div class="img" style={`opacity: ${$l1 ? 1 : 0}`}>
+        <div class="icon" style="opacity: {opacity}">
+            <Fa fw icon={icon}/>
+        </div>
+    </div>
+    <div class="texts" style="top: {parcent}%">
+        <p style={`opacity: ${$l1 ? 1 : 0}`}>For Advanded User</p>
+        <p style={`opacity: ${$l2 ? 1 : 0}`}>80+ Roles</p>
+        <p style={`opacity: ${$l3 ? 1 : 0}`}>700+ Options</p>
     </div>
 </div>
 
 <style>
     .user {
-        height: 1500px;
+        height: 2000px;
         display: flex;
         justify-content: center;
         align-items: center;
     }
     .texts {
-        display: flex;
-        justify-content: center;
-        align-items: center;
         position: fixed;
-        left: 50%;
-        top: 50%;
+        left: 60%;
         transform: translateX(-50%);
         font-family: 'Inter', sans-serif;
         font-weight: 600;
-        font-size: 70px;
+        font-size: 40px;
+        overflow: hidden;
+        transition: all 0.3s ease;
     }
     .texts p {
         transition: all 0.4s ease;
         margin-inline: 10px;
+    }
+    .img {
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        left: 44%;
+        top: 42%;
+        width: 12rem;
+        height: 12rem;
+        background: #282828;
+        border-radius: 10px;
+        transform: translateX(-50%);
+        font-family: 'Inter', sans-serif;
+        font-size: 100px;
+        transition: all 0.3s ease;
+    }
+    .icon {
+        transition: all 0.5s ease;
     }
 </style>
