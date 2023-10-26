da5495e5ff5e6fc8e355fc66e11e2b35e60244c4
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-10-27 02:21:31 +0900

    change: bio switch able

--- src/components/top/title.svelte
+++ src/components/top/title.svelte
@@ -1,21 +1,43 @@
 <script>
     import { onMount } from "svelte";
+
+    const featureArray = [
+        "Roles",
+        "Experience",
+        "Setting",
+        "Performance",
+    ];
+
     let titleOpacity = 0;
     let bioOpacity = 0;
-    onMount(() =>
+
+    let currentIndex = 0;
+    let isVisible = true;
+
+    onMount(() => {
         setTimeout(() => {
             titleOpacity = 1
             setTimeout(() => {
                 bioOpacity = 1
             }, 300)
         }, 150)
-    );
+        setInterval(() => {
+            isVisible = false;
+            setTimeout(() => {
+                currentIndex = (currentIndex + 1) % featureArray.length;
+                isVisible = true;
+            }, 600);
+        }, 1500);
+    });
+    // TODO: featureArrayの切り替えをアニメーションのホーイール的なものにしたい
 </script>
 
 <div class="title">
     <div class="content">
         <p class="big" style="opacity: {titleOpacity}">Extreme Roles</p>
-        <p class="bio" style="opacity: {bioOpacity}">Extreme Experience for your AmongUs</p>
+        <p class="bio" style="opacity: {bioOpacity}">
+            Extreme {featureArray[currentIndex]} for your AmongUs
+        </p>
     </div>
 </div>
 
@@ -37,7 +59,7 @@
     .bio {
         font-family: 'Inter', sans-serif;
         width: 100%;
-        margin-block: 10px;
+        margin: 25px 0px 10px 0px;
         font-size: 20px;
         transition: all 0.5s ease;
         color: #797878;
